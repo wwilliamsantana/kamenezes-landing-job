3e1e8ee3333c9d0db1ab50bf55c82dbef3c73556
William Santana <euwilliamsantana@gmail.com>
2025-07-07 23:40:56 -0300

    refactor: Button Hero Link

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
         <h1 className="text-3xl font-bold" >Ajudo mulheres a se livrar das estrias.</h1>
         <p className="text-sm">Descubra o poder dos tratamentos que estimulam o colágeno, reduzem marcas e devolvem a confiança que você merece.</p>
       </div>
-      <a href="https://www.instagram.com/karen.msantana/" target="_blank">
+      <a href="https://wa.link/jih31k" target="_blank">
         <button className="bg-stone-500 text-sm py-3 px-4 text-white rounded-md hover:bg-stone-600 font-bold cursor-pointer">Agende agora sua avaliação grátis</button>
       </a>
 
