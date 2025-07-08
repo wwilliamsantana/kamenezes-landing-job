a25a299c0cf6edaee74378d0c9c4a8d9220f7b43
William Santana <euwilliamsantana@gmail.com>
2025-07-08 00:00:07 -0300

    refactor: responsive

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,13 +1,13 @@
 export function Contacts() {
   return (
-    <article className=" py-20 w-1/2 max-w-7xl mx-auto text-stone-700  flex flex-col items-center justify-center gap-8 max-lg:w-4/6">
+    <article className=" py-20 w-1/2 max-w-7xl mx-auto text-stone-700  flex flex-col items-center justify-center gap-8 max-lg:w-4/6 px-2">
       <h2 className="font-bold text-2xl text-center ">Dê o Primeiro Passo Rumo à Sua Pele Ideal!</h2>
       <p >Não deixe que as estrias afetem sua autoestima. Estou pronta para te ajudar a conquistar a pele que você sempre desejou. Agende sua avaliação e descubra o plano de tratamento personalizado para você.</p>
-      <div className="flex flex-col items-center gap-2 p-2">
+      <div className="flex flex-col items-center gap-2 justify-center w-full">
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125333.72436699877!2d-37.20798820273439!3d-10.987592599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71ab171eee2821b%3A0xd375a8d5b0aa305a!2sSuellen%20Rocha%20Cl%C3%ADnica%20Micropigmenta%C3%A7%C3%A3o%2C%20Est%C3%A9tica%2C%20Remo%C3%A7%C3%A3o%2C%20Design%20de%20Sobrancelhas%20e%20Sal%C3%A3o%20de%20beleza%20Aracaju!5e0!3m2!1spt-BR!2sbr!4v1751937745958!5m2!1spt-BR!2sbr" className="border-0 w-2xl h-64 max-md:w-lg max-sm:w-sm" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-        <span className="text-xs font-bold">Endereço do consultório: Av. Poeta Vinícius de Moraes, 70 - Atalaia, Aracaju - SE</span>
-        <span className="text-xs font-bold ">Horário de Atendimento: Segunda a Sexta - 14:30h às 20h - Sábado - 9h às 17h</span>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125333.72436699877!2d-37.20798820273439!3d-10.987592599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71ab171eee2821b%3A0xd375a8d5b0aa305a!2sSuellen%20Rocha%20Cl%C3%ADnica%20Micropigmenta%C3%A7%C3%A3o%2C%20Est%C3%A9tica%2C%20Remo%C3%A7%C3%A3o%2C%20Design%20de%20Sobrancelhas%20e%20Sal%C3%A3o%20de%20beleza%20Aracaju!5e0!3m2!1spt-BR!2sbr!4v1751937745958!5m2!1spt-BR!2sbr" className="border-0 w-2xl h-64 max-md:w-lg max-sm:w-sm max-[420px]:hidden" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <span className="text-xs font-bold w-full">Endereço do consultório: Av. Poeta Vinícius de Moraes, 70 - Atalaia, Aracaju - SE</span>
+        <span className="text-xs font-bold w-full ">Horário de Atendimento: Segunda a Sexta - 14:30h às 20h - Sábado - 9h às 17h</span>
       </div>
     </article>
   )
--- src/components/result.tsx
+++ src/components/result.tsx
@@ -5,21 +5,21 @@ export function Results() {
     <article className="p-16 bg-stone-300 max-w-7xl mx-auto flex justify-around items-center flex-col rounded-md gap-10 max-sm:flex-col">
       <h2 className="font-bold text-2xl text-center"> Cada estria tem uma cor. E nós temos o tratamento certo para cada uma!</h2>
       <div className="flex justify-center gap-6 w-full max-lg:flex-wrap">
-        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80  ">
+        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80  max-sm:w-48 max-sm:h-64">
           <img src={"/estrias-bra.png"} className="object-cover w-full h-full" />
-          <p className="font-bold text-sm uppercase">Branca</p>
+          <p className="font-bold text-sm uppercase p-2">Branca</p>
         </div>
-        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80 ">
+        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80 max-sm:w-48 max-sm:h-64">
           <img src={"/estrias-verm.png"} className="object-cover w-full h-full" />
-          <p className="font-bold text-sm uppercase">Vermelho</p>
+          <p className="font-bold text-sm uppercase p-2">Vermelho</p>
         </div>
-        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80 ">
+        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80 max-sm:w-48 max-sm:h-64">
           <img src={"/estrias-marron.png"} className="object-cover w-full h-full" />
-          <p className="font-bold text-sm uppercase">Marrom</p>
+          <p className="font-bold text-sm uppercase p-2">Marrom</p>
         </div>
-        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80 ">
+        <div className="flex flex-col items-center justify-center gap-3 shadow-2xl rounded-lg overflow-hidden w-64 h-80 max-sm:w-48 max-sm:h-64">
           <img src={"/estrias-roxa.png"} className="object-cover w-full h-full" />
-          <p className="font-bold text-sm uppercase">Roxa</p>
+          <p className="font-bold text-sm uppercase p-2">Roxa</p>
         </div>
 
       </div>
